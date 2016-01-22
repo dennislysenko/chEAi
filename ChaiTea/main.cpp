b4f292338c2362da3130372838795ff582a19f8d
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2016-01-22 04:26:07 -0500

    Initial Commit

---
+++ ChaiTea/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChaiTea
+//
+//  Created by Dennis Lysenko on 1/22/16.
+//  Copyright © 2016 Dennis Lysenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
